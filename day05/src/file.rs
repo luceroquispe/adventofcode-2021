ed87efc2ffdbeb29174990f41af9f03d5b5c57e3
luceroquispe <Lucero.Quispe@proton.me>
2024-01-04 23:39:23 +1100

    feat: read output to stdout, tests and tidy

--- day05/src/file.rs
+++ day05/src/file.rs
@@ -9,40 +9,38 @@
 /// The module also includes tests for:
 ///     * `Coordinate` struct
 ///     * `parse_line` function
+///     * `parse_file_to_points_pairs`
 ///     * `read_file_to_points` function
 ///
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{self, BufRead, BufReader};
 
 use crate::common::Coordinate;
 
-fn parse_line(input: String) -> Result<Vec<Coordinate>, std::num::ParseIntError> {
-    input
-        .split("->")
-        .map(|coords| coords.trim())
-        .filter(|coords| !coords.is_empty())
-        .map(|coords| coords.parse())
-        .collect()
-}
-
-fn parse_file_to_points_pairs(
-    reader: BufReader<File>,
-) -> Result<Vec<Vec<Coordinate>>, Box<dyn std::error::Error>> {
-    let mut point_pairs = Vec::new();
-    for line_result in reader.lines() {
-        let line = line_result?;
-        let points = parse_line(line)?;
-        point_pairs.push(points);
+fn parse_line(input: &str) -> Option<(Coordinate, Coordinate)> {
+    let coords: Vec<&str> = input.split("->").collect();
+    if coords.len() != 2 {
+        return None;
     }
-    Ok(point_pairs)
+
+    let start: Coordinate = coords[0].trim().parse().ok()?;
+    let end: Coordinate = coords[1].trim().parse().ok()?;
+
+    if start.row == end.row || start.col == end.col {
+        Some((start, end))
+    } else {
+        None
+    }
 }
 
-pub fn read_file_to_points(
-    path: &String,
-) -> Result<Vec<Vec<Coordinate>>, Box<dyn std::error::Error>> {
+pub fn read_file_to_points(path: &str) -> io::Result<Vec<(Coordinate, Coordinate)>> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
-    parse_file_to_points_pairs(reader)
+    let point_pairs: Vec<_> = reader
+        .lines()
+        .filter_map(|line| line.ok().and_then(|l| parse_line(&l)))
+        .collect();
+    Ok(point_pairs)
 }
 
 #[cfg(test)]
@@ -55,7 +53,7 @@ mod tests {
         let coord_str = "0,9";
         let coord = coord_str.parse::<Coordinate>();
         assert!(coord.is_ok());
-        assert_eq!(coord.unwrap(), Coordinate { x: 0, y: 9 });
+        assert_eq!(coord.unwrap(), Coordinate { row: 0, col: 9 });
 
         // Test case 2
         let coord_str = "5,9";
@@ -63,56 +61,47 @@ mod tests {
         assert!(coord.is_ok(), "Failed to parse coordinate: {}", coord_str);
         assert_eq!(
             coord.unwrap(),
-            Coordinate { x: 5, y: 9 },
+            Coordinate { row: 5, col: 9 },
             "Coordinate parsed but not equal to expected value"
         );
     }
 
     #[test]
-    fn test_parse_line() {
+    fn test_parse_line_ok() {
         // Test case 1
         let line = "0,9 -> 5,9";
-        let result = parse_line(line.to_string());
-        assert!(result.is_ok());
+        let result = parse_line(line);
         assert_eq!(
             result.unwrap(),
-            vec![Coordinate { x: 0, y: 9 }, Coordinate { x: 5, y: 9 }],
+            (Coordinate { row: 0, col: 9 }, Coordinate { row: 5, col: 9 }),
             "Failed to parse line: {}",
             line
-        );
-
-        // Test case 2
-        let line = "8,0 -> 0,8";
-        let result = parse_line(line.to_string());
-        assert!(result.is_ok());
-        assert_eq!(
-            result.unwrap(),
-            vec![Coordinate { x: 8, y: 0 }, Coordinate { x: 0, y: 8 }],
-            "Failed to parse line: {}",
-            line
-        );
+        )
     }
 
     #[test]
-    fn test_read_file_to_points() {
-        // Test case 1
+    fn test_parse_line_return_none() {
+        // Test case 2
+        let line = "8,0 -> 0,8";
+        let result = parse_line(line);
+        assert!(result.is_none(), "Expected None, but got Some");
+    }
+
+    #[test]
+    fn test_read_file_to_points_dropping_diagonals() {
+        // missing a few records since they aren't straight lines
         let path = "data/sample1.txt";
         let result = read_file_to_points(&path.to_string());
         assert!(result.is_ok());
         assert_eq!(
             result.unwrap(),
             vec![
-                [Coordinate { x: 0, y: 9 }, Coordinate { x: 5, y: 9 }],
-                [Coordinate { x: 8, y: 0 }, Coordinate { x: 0, y: 8 }],
-                [Coordinate { x: 9, y: 4 }, Coordinate { x: 3, y: 4 }],
-                [Coordinate { x: 2, y: 2 }, Coordinate { x: 2, y: 1 }],
-                [Coordinate { x: 7, y: 0 }, Coordinate { x: 7, y: 4 }],
-                [Coordinate { x: 6, y: 4 }, Coordinate { x: 2, y: 0 }],
-                [Coordinate { x: 0, y: 9 }, Coordinate { x: 2, y: 9 }],
-                [Coordinate { x: 3, y: 4 }, Coordinate { x: 1, y: 4 }],
-                [Coordinate { x: 0, y: 0 }, Coordinate { x: 8, y: 8 }],
-                [Coordinate { x: 5, y: 5 }, Coordinate { x: 8, y: 2 }]
-            ]
+                (Coordinate { row: 0, col: 9 }, Coordinate { row: 5, col: 9 }),
+                (Coordinate { row: 9, col: 4 }, Coordinate { row: 3, col: 4 }),
+                (Coordinate { row: 2, col: 2 }, Coordinate { row: 2, col: 1 }),
+                (Coordinate { row: 7, col: 0 }, Coordinate { row: 7, col: 4 }),
+                (Coordinate { row: 0, col: 9 }, Coordinate { row: 2, col: 9 }),
+                (Coordinate { row: 3, col: 4 }, Coordinate { row: 1, col: 4 })]
         )
     }
 }
--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,19 +1,13 @@
 /*
 TODO: Read file into points
 */
-use day05::common::Coordinate;
-use day05::grid::{Grid, GridOperations};
 use day05::file::read_file_to_points;
+use day05::grid::Grid;
 
 fn main() {
-    let mut grid = Grid::default();
-    let point = Coordinate { x: 1, y: 1 };
-    grid.add_point(point);
-    println!("{}", grid);
-    let count = grid.get_count(&point).unwrap();
-    println!("{}", count);
     let path = String::from("data/sample1.txt");
     let points = read_file_to_points(&path).unwrap();
     print!("{:?}", points);
-
+    let grid = Grid::new(points);
+    println!("{}", grid)
 }
